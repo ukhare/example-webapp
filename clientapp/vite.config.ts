cd166eb2573b0e26255afc9a79827c2147ab2e3d
UJJAWAL KHARE <ukhare@outlook.com>
2024-12-13 14:34:22 +0530

    Update vite.config.ts

--- clientapp/vite.config.ts
+++ clientapp/vite.config.ts
@@ -1,5 +1,24 @@
-import { defineConfig } from 'vite' // Missing semicolon will generate a warning
-import react from '@vitejs/plugin-react'
+//import { defineConfig } from 'vite' // Missing semicolon will generate a warning
+//import react from '@vitejs/plugin-react'
+
+//// https://vitejs.dev/config/
+//export default defineConfig({
+//  base: '/app',
+//  server: {
+//    https: false,
+//    port: 5100,
+//    strictPort: true,
+//    proxy: {
+//      // Missing closing bracket will generate a syntax error
+//      '/api': {
+//        target: 'http://localhost:3000',
+//        changeOrigin: true
+//    } 
+//  },
+//  plugins: [react()],
+//})
+import { defineConfig } from 'vite'; // Added missing semicolon
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,11 +28,11 @@ export default defineConfig({
     port: 5100,
     strictPort: true,
     proxy: {
-      // Missing closing bracket will generate a syntax error
       '/api': {
         target: 'http://localhost:3000',
         changeOrigin: true
-    } 
+      } // Added closing bracket
+    }
   },
   plugins: [react()],
-})
+});
